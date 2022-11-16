ffb16b5666f81542302fd7060170fd4be517a885
Sergey <segarik93@gmail.com>
2022-11-16 23:01:10 +0300

    zada4a 2 end

--- zada4a2/Program.cs
+++ zada4a2/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Введите три числа:");
+
+int a, b, c;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+int maxnum = a;
+if (a > maxnum) maxnum = a;
+if (b > maxnum) maxnum = b;
+if (c > maxnum) maxnum = c; 
+{
+Console.WriteLine($"Максимальное {maxnum}");
+}
