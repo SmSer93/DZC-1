9db82b7385e654909b2bdfe689dac6e687ac0d91
Sergey <segarik93@gmail.com>
2022-11-16 22:27:42 +0300

    dz1 end

--- zada4a1/Program.cs
+++ zada4a1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите два числа:");
+
+int a, b;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+ {
+Console.WriteLine($"Max = {a}");
+}
+
+if (b > a)
+    { 
+ Console.WriteLine($"Max = {b}");
+}
